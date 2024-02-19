b45d28d851ef48e50893a3f582633cf6e7d1ab5d
andreamusso96 <andreamusso96@gmail.com>
2024-02-19 12:54:53 -0500

    Backboning: shift from graphtool to network x as backend for network utils

--- src/network_utils/__init__.py
+++ src/network_utils/__init__.py
@@ -1,4 +1,3 @@
 from . converter import convert_graph, GraphType
-from . adjacency import undirected_weighted_graph_from_weight_matrix, directed_weighted_graph_from_weight_matrix
 from . backboning import get_network_backbone, BackBoneMethod, BackBoneResult
 from . community_detection import run_community_detection, Algorithm as CommunityDetectionAlgorithm, CommunityDetectionResult
--- src/network_utils/backboning.py
+++ src/network_utils/backboning.py
@@ -1,48 +1,37 @@
-from typing import Dict, Any
+from typing import Dict, Any, Union
 from enum import Enum
 
 import numpy as np
 import pandas as pd
-import graph_tool.all as gt
+import networkx as nx
 import plotly.graph_objects as go
-
-from . import adjacency
 
 
 class BackBoneMethod(Enum):
     NO = 'no_filter'
     DISPARITY = 'disparity'
     MAXIMUM_SPANNING_TREE = 'maximum_spanning_tree'
+    DISPARITY_AND_MAXIMUM_SPANNING_TREE = 'disparity_and_maximum_spanning_tree'
 
 
 class BackBoneResult:
-    def __init__(self, g: gt.Graph, backbone_g: gt.Graph, method: BackBoneMethod, kwargs: Dict[str, Any] = None):
+    def __init__(self, g: nx.Graph, backbone_g: nx.Graph, method: BackBoneMethod, kwargs: Dict[str, Any] = None):
         self.g = g
+        self.adjacency_g = pd.DataFrame(nx.adjacency_matrix(g, weight='weight').toarray(), index=g.nodes, columns=g.nodes)
         self.backbone_g = backbone_g
+        self.backbone_adjacency_g = pd.DataFrame(nx.adjacency_matrix(backbone_g, weight='weight').toarray(), index=backbone_g.nodes, columns=backbone_g.nodes)
         self.method = method
         self.kwargs = kwargs
 
-    def plot_heterogeneity_of_edge_weights(self):
-        weighted_adjacency_graph = gt.adjacency(self.g, weight=self.g.ep['weight']).toarray().astype(float)
-        node_strengths = weighted_adjacency_graph.sum(axis=1)
-        normalized_adjacency_graph = np.divide(weighted_adjacency_graph, node_strengths, out=np.zeros_like(weighted_adjacency_graph), where=node_strengths != 0)
-        degrees = (weighted_adjacency_graph > 0).sum(axis=1)
-        heterogeneity = degrees * np.sum(np.square(normalized_adjacency_graph), axis=1)
-        fig = go.Figure(data=go.Scatter(x=degrees, y=heterogeneity, mode='markers'))
-        fig.update_layout(title=f'Heterogeneity of edge weights for {self.method.value} backbone')
-        fig.update_xaxes(title='Degree', type='log')
-        fig.update_yaxes(title='Heterogeneity', type='log')
-        fig.show(renderer='browser')
-
     def plot_summary_table(self):
-        total_weight_original_graph = np.sum(np.array(self.g.ep['weight'].a))
-        total_weight_backbone_graph = np.sum(np.array(self.backbone_g.ep['weight'].a))
+        total_weight_original_graph = np.sum(self.adjacency_g)
+        total_weight_backbone_graph = np.sum(self.backbone_adjacency_g)
 
-        total_edges_original_graph = self.g.num_edges()
-        total_edges_backbone_graph = self.backbone_g.num_edges()
+        total_edges_original_graph = self.g.number_of_edges()
+        total_edges_backbone_graph = self.backbone_g.number_of_edges()
 
-        total_node_connected_components_original_graph = self.g.num_vertices()
-        total_node_connected_components_backbone_graph = gt.extract_largest_component(self.backbone_g, directed=False).num_vertices()
+        total_node_connected_components_original_graph = self.g.number_of_nodes()
+        total_node_connected_components_backbone_graph = np.max([len(a) for a in nx.connected_components(self.backbone_g)])
 
         average_degree_original_graph = total_edges_original_graph / total_node_connected_components_original_graph
         average_degree_backbone_graph = total_edges_backbone_graph / total_node_connected_components_backbone_graph
@@ -57,7 +46,7 @@ class BackBoneResult:
                                [total_weight_backbone_graph, total_edges_backbone_graph, total_node_connected_components_backbone_graph, np.round(average_degree_backbone_graph, decimals=3)],
                                [np.round(total_weight_backbone_graph / total_weight_original_graph, decimals=3),
                                 np.round(total_edges_backbone_graph / total_edges_original_graph, decimals=3),
-                               np.round(total_node_connected_components_backbone_graph / total_node_connected_components_original_graph, decimals=3),
+                                np.round(total_node_connected_components_backbone_graph / total_node_connected_components_original_graph, decimals=3),
                                 np.round(average_degree_backbone_graph / average_degree_original_graph, decimals=3)]],
                        line_color='darkslategray',
                        fill_color='lightcyan',
@@ -66,55 +55,52 @@ class BackBoneResult:
         table.show(renderer='browser')
 
 
-def get_network_backbone(g: gt.Graph, method: BackBoneMethod, **kwargs) -> BackBoneResult:
-    assert 'id' in g.vp, 'Graph does not have node ids'
-    assert 'weight' in g.ep, 'Graph does not have edge weights'
+def get_network_backbone(adj: pd.DataFrame = None, g: nx.Graph = None, method: BackBoneMethod = BackBoneMethod, directed: bool = None, **kwargs) -> BackBoneResult:
+    assert adj is not None or g is not None, 'Either adjacency or graph must be provided'
+    assert adj is None or g is None, 'Either adjacency or graph must be provided, but not both'
 
+    if adj is not None:
+        assert directed is not None, 'Directed must be provided when adjacency is provided'
+        assert np.max(np.diag(adj.values)) == 0, 'Adjacency matrix has self-loops, no self-loops are allowed'
+        return _get_network_backbone_from_adjacency(adj=adj, method=method, directed=directed, **kwargs)
+    else:
+        return _get_network_backbone_from_graph(g=g, method=method, **kwargs)
+
+
+def _get_network_backbone_from_adjacency(adj: pd.DataFrame, method: BackBoneMethod, directed: bool, **kwargs) -> BackBoneResult:
+    if directed:
+        g = nx.from_pandas_adjacency(adj, create_using=nx.DiGraph)
+    else:
+        g = nx.from_pandas_adjacency(adj, create_using=nx.Graph)
+
+    return _get_network_backbone_from_graph(g=g, method=method, **kwargs)
+
+
+def _get_network_backbone_from_graph(g: nx.Graph, method: BackBoneMethod, **kwargs) -> BackBoneResult:
     if method == BackBoneMethod.NO:
         backbone_g = g
     elif method == BackBoneMethod.DISPARITY:
         backbone_g = disparity_edge_filter(g=g, **kwargs)
-    elif method == BackBoneMethod.MAXIMUM_SPANNING_TREE:
-        backbone_g = maximum_spanning_tree_edge_filter(g=g)
     else:
         raise ValueError(f'Unknown backbone method {method}')
 
     return BackBoneResult(g=g, backbone_g=backbone_g, method=method, kwargs=kwargs)
 
 
-def disparity_edge_filter(g: gt.Graph, alpha: float = 0.05) -> gt.Graph:
-    weighted_adjacency_graph = gt.adjacency(g, weight=g.ep['weight']).toarray().astype(float)
-    p_values_in_edges = _compute_pvalues_edges(weighted_adjacency_graph=weighted_adjacency_graph)
-    p_values_out_edges = _compute_pvalues_edges(weighted_adjacency_graph=weighted_adjacency_graph.T)
+def disparity_edge_filter(g: nx.Graph, alpha: float = 0.05) -> nx.Graph:
+    weighted_adjacency_graph = nx.adjacency_matrix(g, weight='weight').toarray()
+    p_values_in_edges = _compute_pvalues_edges(weighted_adjacency_graph=weighted_adjacency_graph.T)
+    p_values_out_edges = _compute_pvalues_edges(weighted_adjacency_graph=weighted_adjacency_graph)
     p_values_edges = np.minimum(p_values_in_edges, p_values_out_edges)
     adjacency_filtered_graph = p_values_edges < alpha
     weighted_adjacency_filtered_graph = np.multiply(weighted_adjacency_graph, adjacency_filtered_graph)
-    filtered_graph = _get_filtered_graph_from_weighted_adjacency_filtered_graph(weighted_adjacency_filtered_graph=weighted_adjacency_filtered_graph, vertex_ids=np.array(g.vp['id'].a),
-                                                                                filter_name='disparity', directed=g.is_directed())
+    filtered_graph = nx.from_pandas_adjacency(pd.DataFrame(weighted_adjacency_filtered_graph, index=g.nodes, columns=g.nodes), create_using=type(g))
     return filtered_graph
 
-
-def maximum_spanning_tree_edge_filter(g: gt.Graph) -> gt.Graph:
-    maximum_spanning_tree = gt.min_spanning_tree(g, weights=g.new_edge_property('double', vals=-1*g.ep['weight'].a))
-    filtered_graph = gt.Graph(gt.GraphView(g, efilt=maximum_spanning_tree), prune=True)
-    filtered_graph.gp['filter'] = filtered_graph.new_graph_property(value_type="string", val='maximum_spanning_tree')
-    return filtered_graph
-
-def _compute_pvalues_edges(weighted_adjacency_graph: np.ndarray):
+def _compute_pvalues_edges(weighted_adjacency_graph: np.ndarray) -> np.ndarray:
     node_strengths = weighted_adjacency_graph.sum(axis=1).reshape(-1, 1)
     normalized_adjacency_graph = np.divide(weighted_adjacency_graph, node_strengths, out=np.zeros_like(weighted_adjacency_graph), where=node_strengths != 0)
     degrees = (weighted_adjacency_graph > 0).sum(axis=1)
     degrees_min_one = degrees - 1
     p_values_edges = np.power(1 - normalized_adjacency_graph, degrees_min_one, out=np.ones_like(weighted_adjacency_graph), where=degrees_min_one > 0)
     return p_values_edges
-
-
-def _get_filtered_graph_from_weighted_adjacency_filtered_graph(weighted_adjacency_filtered_graph: np.ndarray, vertex_ids: np.ndarray, filter_name: str, directed: bool) -> gt.Graph:
-    weighted_adjacency_filtered_graph = pd.DataFrame(weighted_adjacency_filtered_graph, index=vertex_ids, columns=vertex_ids)
-    if directed:
-        filtered_graph = adjacency.directed_weighted_graph_from_weight_matrix(weight_matrix=weighted_adjacency_filtered_graph)
-    else:
-        filtered_graph = adjacency.undirected_weighted_graph_from_weight_matrix(weight_matrix=weighted_adjacency_filtered_graph)
-
-    filtered_graph.gp['filter'] = filtered_graph.new_graph_property(value_type="string", val=filter_name)
-    return filtered_graph
