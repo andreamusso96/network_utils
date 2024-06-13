3bb87f3ec59e7fa555ec0173f49b4943ea289112
andreamusso96 <andreamusso96@gmail.com>
2024-06-13 14:45:29 +0200

    Added monopartite with rca

--- src/network_utils/__init__.py
+++ src/network_utils/__init__.py
@@ -1,3 +1,4 @@
 from . converter import convert_graph, GraphType
 from . backboning import get_network_backbone, BackBoneMethod, BackBoneResult
 from . community_detection import run_community_detection, Algorithm as CommunityDetectionAlgorithm, CommunityDetectionResult
+from . projection import monopartite_projection_rca
